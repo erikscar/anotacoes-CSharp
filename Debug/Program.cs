de14eb05cf48e572569d320a38c49669ddb5aa01
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-02-07 12:06:12 -0300

    Variables

---
+++ Debug/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//Debug = Procurando algum Erro, tem que ser .cs, clicar na bolinha vermelha,
+//para adicionar um Break point, uma parada na execução
+//Porém temos que entrar no MODO DEBUG UTILIZAR O PLAY LA NA ESQUERDA DO VS CODE
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Testando";
+            Console.WriteLine(texto);
+        }
+    }
+}
+
---
+++ Variáveis/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Variaveis
+{
+    //VARIAVEIS PODEM SER ALTERADAS E CONSTANTES NÃO
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int idade; Se não iniciarmos, será 0
+            //int idade = 25; Iniciando a variável como 25
+            var idade = 25; // Iniciando a variável como 25
+                            //var idade; Erro: var deve ser inicializado
+
+            //  const var idade = 25; NÃO É POSSÍVEL UTILIZAR CONSTANTE + O VAR  
+            // const int IDADE_MINIMA = 30; Iniciando uma constante com o valor 30
+
+            byte myByte = 127; //Tipo Byte é bem pequeno, sbyte para usar numeros negativos
+
+            //Números Inteiros, podem receber numeros negativos, colocando o "u" na frente não podem mais
+            short myShort = 4;
+            int myInt = 10;
+            long myLong = 200;
+
+            //Número Reais ou Flutuantes, com ponto NÃO POSSUI UNSIGNED OU SIGNED (N Inteiros o N Negativos )
+            float myFloat = 4.5f; //Tem que colocar um f no final
+            double myDouble = 9.8000; //Não coloca nada pq é o PADRÃO
+            decimal myDecimal = 24.3m; //tem que colocar um m no final
+
+            //VAR = Armazenar Qualquer tipo de Dado, ele infere um automaticamente
+            var teste = 2.403;
+            var teste1 = 1;
+            
+            //BOOL = Armazenar valores booleanos (false/true)
+            bool verdade = false; 
+            bool falso = true;
+
+            //CHAR = Armazenar apenas um caracter (FORMATO UNICODE)
+            char myChar = 'a'; //Aspas Simples!!!
+        }
+    }
+}
+
+
+
