30b5c032664098e46932ae4857fb8e5729011ab8
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-03-07 15:10:53 -0300

    Estruturas Condicionais e Laços de Repetição pt1

---
+++ Estruturas Condicionais/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IF = "SE", tomada de decisão utilizando operadores lógicos
+            //ELSE = "SE NÃO", caso a condição não seja atendida no IF
+            //ELSE IF "Continua com o if, não tem limites, não é muito agradável
+
+            // int x = 1;
+            // if (x == 0)
+            // {
+            //     Console.WriteLine("Verdadeiro");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Falso");
+            // }
+
+            //SWITCH, Quando há muitas decisões
+
+            int y = 3;
+            switch (y)
+            {
+                case 1: Console.WriteLine("1"); break;
+                case 2: Console.WriteLine("2"); break;
+                case 3: Console.WriteLine("3"); break;
+                default: Console.WriteLine("Nenhum dos 3 Primeiros!"); break;
+            }
+
+
+        }
+    }
+}
---
+++ Laços de Repetição/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For, Executar uma tarefa várias vezes
+            // for (int i = 0; i < 11; i++) Console.WriteLine(i);
+            // for (int i = 5; i >= 0; i--) Console.WriteLine(i);
+
+            int x = 5;
+            //While, Exige uma condição para ser iniciado
+            while (x != 0)
+            {
+                Console.WriteLine(x);
+                x--;
+            }
+        }
+    }
+}
