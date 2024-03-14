d5cd498267d99822b6446d98e6ac7f31b0b9b6d7
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-03-14 15:15:37 -0300

    Início do Timer do Projeto de Cronômetro

---
+++ Cronômetro/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Projetos
+{
+    class Stopwatch
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.WriteLine("Aeblha");
+                currentTime++;
+            }
+        }
+    }
+}
