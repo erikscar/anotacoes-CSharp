967feab5fc0b44749f64397b132dd400cc432c88
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-03-20 09:51:35 -0300

    Função de Salvar utilizando StreamWriter

--- Projetos/TextEditor/Program.cs
+++ Projetos/TextEditor/Program.cs
@@ -1,12 +1,16 @@
 ﻿
 
-namespace CSHARP {
-  class Program {
-    static void Main(string[] args) {
+namespace CSHARP
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
       Menu();
     }
 
-    static void Menu() {
+    static void Menu()
+    {
       Console.Clear();
       Console.WriteLine("Bem Vindo(a) ao Editor de Texto \n");
       Console.WriteLine("1) Abrir Arquivo");
@@ -14,42 +18,60 @@ namespace CSHARP {
       Console.WriteLine("0) Sair");
       short res = short.Parse(Console.ReadLine());
 
-      switch(res) {
-        case 0 : {
-          Console.WriteLine("Encerrando o Programa...");
-          Environment.Exit(0);
-          break; 
-        }
-        case 1 : Open(); break;
-        case 2 : Edit(); break;
-        default: {
-          Console.WriteLine("Opção Inválida...");
-          Menu();
-          break; 
-        } 
+      switch (res)
+      {
+        case 0:
+          {
+            Console.WriteLine("Encerrando o Programa...");
+            Environment.Exit(0);
+            break;
+          }
+        case 1: Open(); break;
+        case 2: Edit(); break;
+        default:
+          {
+            Console.WriteLine("Opção Inválida...Retornando ao Menu");
+            Menu();
+            break;
+          }
       }
     }
-    static void Open() {}
-    static void Edit() {
+    static void Open() { }
+    static void Edit()
+    {
       Console.Clear();
       Console.WriteLine("Digite Seu Texto: (Pressione ESC para Confirmar)");
       Console.WriteLine("----------------------------\n");
       string text = "";
 
-      do {
+      do
+      {
         text += Console.ReadLine();
         text += Environment.NewLine; //Nova Linha
-      } while(Console.ReadKey().Key != ConsoleKey.Escape); //Enquanto a tecla Enter não for pressionada
+      } while (Console.ReadKey().Key != ConsoleKey.Escape); //Enquanto a tecla Enter não for pressionada
+
+      Save(text);
     }
- 
-    static void Save(string text) {
+
+    static void Save(string text)
+    {
       Console.Clear();
-      Console.WriteLine("Qual Caminho Salvar?");
+      Console.WriteLine("Qual Caminho Salvar o Arquivo?");
       var path = Console.ReadLine();
 
-      using var file = new StreamWriter(path); //Qualquer Objeto dentro do Using é utilizado depois é fechado
-      file.Write(text);
-      Console.WriteLine("Arquivo Salvo com Sucesso!");
+      //Manipular Arquivos = StreamWriter
+
+      using (var file = new StreamWriter(path))//StreamWriter Sempre pede o Caminho do Arquivo
+                                               //Qualquer Objeto dentro do Using é utilizado depois é fechado de forma AUTOMÁTICA
+                                               //Todo Arquivo deve ter seu fechamento, se não ocasiona erros
+
+      {
+        file.Write(text);
+      }
+
+      Console.WriteLine($"Arquivo {path} Salvo com Sucesso!");
+      Console.ReadKey();
+      Menu();
     }
   }
 }
