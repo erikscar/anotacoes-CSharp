a91a6b21de44ef7e25168127194dc399ccf4ce81
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-03-20 14:27:03 -0300

    Adicionado Anotações sobre Comaração de Strings

---
+++ Comparação_de_Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "testando";
+
+            Console.WriteLine(text.CompareTo("testando"));
+            //0 = String Igual
+            Console.WriteLine(text.CompareTo("Testando"));
+            //-1 = String Diferente
+
+            var text1 = "Esse Texto é um Teste";
+
+            Console.WriteLine(text1.Contains("teste"));
+            //Retorna False
+            Console.WriteLine(text1.Contains("Teste"));
+            //Retorna True
+            Console.WriteLine(text1.Contains("Teste", StringComparison.OrdinalIgnoreCase));
+            //Ignorar o Case Sensitive, maiusculo ou minusculo
+
+
+
+
+
+        }
+    }
+}
