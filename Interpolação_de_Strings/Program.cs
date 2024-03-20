2547e099e8a74b5d5abdcc055bd64e86081efee4
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-03-20 14:14:40 -0300

    Adicionado Anotações sobre Interpolação de Strings

---
+++ Interpolação_de_Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Interpolação de Strings (Inserir Valores em uma String)
+
+            var price = 10.2;
+            // var text = "O Preço do Produto é " + price;
+            //Convertido Automaticamente para String
+
+            // var text = string.Format(
+            //     "O Preço do Produto é {0}", price);
+            var text = $@"O Preço
+             do Produto é {price}";
+            //@ Para entender texto com quebra de linha
+            //E ainda ignora caracteres especias como \n
+            Console.WriteLine(text);
+
+        }
+    }
+}
