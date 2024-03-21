b9ca53c360b2f8c7ea1b5268da79dae3da749a31
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-03-21 11:54:05 -0300

    Indices + Métodos Adicionais

---
+++ Índice_de_Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CSHARP
+{
+    class Program {
+        static void Main(string[] args) {
+            var text = "Esse é um Teste" ;
+            //Deve comparar os mesmos tiposde dados
+            Console.WriteLine(text.IndexOf("e"));
+            //Caso a letra se repetir ele irá selecionar o primeiro
+
+            Console.WriteLine(text.LastIndexOf("s"));
+
+            Console.WriteLine(text.ToLower());
+            Console.WriteLine(text.ToUpper());
+
+            //Inserir no ìndice 5 a string "AQUI "
+            Console.WriteLine(text.Insert(5, "AQUI "));
+
+            //Índice para Começar e a Quantidade a se Remover
+            Console.WriteLine(text.Remove(5, 5));
+
+            Console.WriteLine(text.Length);
+        }
+    }
+}
