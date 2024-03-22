403ad4b263a2efeb2625a6ad82c20243c3a0f27e
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-03-22 10:38:27 -0300

    StringBuilder

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace CSHARP {
+    class Program {
+        static void Main(string[] args) {
+            //Ao Fazer isso o Sistema sempre irá criar outro espaço de memória
+            //para a váriavel e para o que foi adicionado! Nem sempre isso é interessante
+            //Quando há textos muito grande
+            var texto = "Este Texto é um Texto";
+            texto += "outro";
+
+            //Para evitar isso usamos o StringBuilder!
+            var text = new StringBuilder();
+            text.Append("Este Texto");
+            text.Append("é um");
+            text.Append("Texto");
+
+            Console.WriteLine(text);
+            
+        }
+    }
+}
